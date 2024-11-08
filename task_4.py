6542fbf01b2ac0ac77d83653fcdd796af18744dc
MU <nikita.feoktistov.2004@mail.ru>
2024-11-09 01:57:59 +0700

    Задача 4: Определение работы

---
+++ task_4.py
@@ -0,0 +1,8 @@
+force = float(input("Введите силу в ньютонах: "))
+distance = float(input("Введите расстояние в метрах: "))
+
+# Вычисляем работу
+work = force * distance
+
+# Выводим результат
+print(f"Работа: {work} джоулей")
